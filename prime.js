b0a134d731d742a59acb8d4fe0c48caaae295929
Mrkishansharma <kishansharma6377@gmail.com>
2022-11-30 20:38:24 +0530

    code created

---
+++ prime.js
@@ -0,0 +1,16 @@
+
+let num=7;
+
+let isPrime = "Yes";
+
+if(num==1 && num==2){
+    console.log(isPrime);
+}else{
+    for(let i=2; i<num; i++){
+        if(num%i == 0){
+            isPrime = "No";
+            break;
+        }
+    }
+    console.log(isPrime);
+}
